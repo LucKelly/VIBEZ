a6abebab80a44648b10461ee54d508ff0314ef3e
Jonas Wahl <jonasjorge97@gmail.com>
2024-06-05 12:57:02 +0100

    fixed navbar, fixed schema and some migartions and added basic mapbox and geocoder functionality

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,2 +1,13 @@
 class BarsController < ApplicationController
+  def index
+    @bars = Bar.all.geocoded
+    @markers = @bars.map do |bar|
+      {
+        lat: bar.latitude,
+        lng: bar.longitude,
+        info_window_html: render_to_string(partial: "bar_popup", locals: { bar: }),
+        marker_html: render_to_string(partial: "marker")
+      }
+    end
+  end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,6 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "ol", to: "https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.9.0/build/ol.js", preload: true
+pin "mapbox-gl", to: "https://ga.jspm.io/npm:mapbox-gl@3.1.2/dist/mapbox-gl.js"
+pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/process-production.js"
