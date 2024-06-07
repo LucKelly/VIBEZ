adcd39eed6c7a89beeffe26c354a67d2c44801b3
Feline Pfeifer <felinepfeifer@icloud.com>
2024-06-07 11:59:50 +0100

    Changed seeds, redirect to map link

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,12 +1,9 @@
+require "uri"
 class BarsController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
   def index
     if params[:query].present?
       @current_params = params[:query]
-      
-
-
-
       @bars = []
       Bar.all.each do |bar|
         bar.main_vibes.include?(params[:query]) ? @bars << bar : nil
@@ -29,9 +26,11 @@ class BarsController < ApplicationController
 
   def show
     @bar = Bar.find(params[:id])
+    query = "https://www.google.com/maps/search/?api=1&query=#{@bar.name} #{@bar.address}"
+    @query_encode = URI::Parser.new.escape(query)
   end
 
-  def show
-    @bar = Bar.find(params[:id])
-  end
+  # def show
+  #   @bar = Bar.find(params[:id])
+  # end
 end
